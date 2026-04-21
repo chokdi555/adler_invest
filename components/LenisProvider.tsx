d3a5f90f37959edc7db229ac6c5d6a85dfca45a0
slava <slavakirsanov905@gmail.com>
2026-04-22 00:58:10 +0200

    Set Lenis lerp to 0.05

--- components/LenisProvider.tsx
+++ components/LenisProvider.tsx
@@ -6,8 +6,7 @@ import Lenis from 'lenis'
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.05,
       smoothWheel: true,
     })
 
